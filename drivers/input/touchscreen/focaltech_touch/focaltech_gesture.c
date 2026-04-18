PixysOS-Devices/kernel_nothing_sm7325

input: touchscreen: focaltech_touch: Implement FOD sensor
Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
Change-Id: I66723dba49a9b17b3c9f461879a20d7522f0f6c1

drivers/input/touchscreen/focaltech_touch/focaltech_gesture.c
@@ -207,6 +207,19 @@ static ssize_t fts_gesture_bm_store(
     return count;
 }
 
+static ssize_t fts_gesture_fod_pressed_show(
+    struct device *dev, struct device_attribute *attr, char *buf)
+{
+    int fp_down = 0;
+    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+
+    mutex_lock(&ts_data->input_dev->mutex);
+    fp_down = ts_data->fod_info.fp_down;
+    mutex_unlock(&ts_data->input_dev->mutex);
+
+    return snprintf(buf, PAGE_SIZE, "%u\n", fp_down);
+}
+
 /* sysfs gesture node
  *   read example: cat  fts_gesture_mode       ---read gesture mode
  *   write example:echo 1 > fts_gesture_mode   --- write gesture mode to 1
@@ -223,10 +236,14 @@ static DEVICE_ATTR(fts_gesture_buf, S_IRUGO | S_IWUSR,
 static DEVICE_ATTR(fts_gesture_bm, S_IRUGO | S_IWUSR,
                    fts_gesture_bm_show, fts_gesture_bm_store);
 
+static DEVICE_ATTR(fts_gesture_fod_pressed, S_IRUGO,
+                   fts_gesture_fod_pressed_show, NULL);
+
 static struct attribute *fts_gesture_mode_attrs[] = {
     &dev_attr_fts_gesture_mode.attr,
     &dev_attr_fts_gesture_buf.attr,
     &dev_attr_fts_gesture_bm.attr,
+    &dev_attr_fts_gesture_fod_pressed.attr,
     NULL,
 };
 
@@ -340,11 +357,13 @@ static void fts_gesture_report(struct fts_ts_data *ts_data,struct input_dev *inp
             ts_data->fod_info.fp_down_report = 1;
             FTS_DEBUG("Gesture Code down=%d", gesture);
             ts_data->fod_gesture_id = gesture;
+            sysfs_notify(&ts_data->dev->kobj, NULL, "fts_gesture_fod_pressed");
             input_report_key(input_dev, gesture, 1);
             input_sync(input_dev);
         } else if ((!ts_data->fod_info.fp_down)&&(ts_data->fod_info.fp_down_report)) {
             ts_data->fod_info.fp_down_report = 0;
             FTS_DEBUG("Gesture Code up=%d", gesture);
+            sysfs_notify(&ts_data->dev->kobj, NULL, "fts_gesture_fod_pressed");
             input_report_key(input_dev, gesture, 0);
             input_sync(input_dev);
         }

TelegramAt25/android_kernel_nothing_mt6886

drivers/input/touchscreen/focaltech_touch/focaltech_gesture.c
@@ -245,6 +245,19 @@ static ssize_t fts_gesture_single_tap_pressed_show(
     return snprintf(buf, PAGE_SIZE, "%u\n", single_tap_pressed);
 }
 
+static ssize_t fts_gesture_fod_pressed_show(
+    struct device *dev, struct device_attribute *attr, char *buf)
+{
+    int fp_down = 0;
+    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+
+    mutex_lock(&ts_data->input_dev->mutex);
+    fp_down = ts_data->fod_info.fp_down_report;
+    mutex_unlock(&ts_data->input_dev->mutex);
+
+    return snprintf(buf, PAGE_SIZE, "%u\n", fp_down);
+}
+
 /* sysfs gesture node
  *   read example: cat  fts_gesture_mode       ---read gesture mode
  *   write example:echo 1 > fts_gesture_mode   --- write gesture mode to 1
@@ -266,12 +279,16 @@ static DEVICE_ATTR(fts_gesture_point, S_IRUGO | S_IWUSR,
 static DEVICE_ATTR(fts_gesture_single_tap_pressed, S_IRUGO,
                    fts_gesture_single_tap_pressed_show, NULL);
 
+static DEVICE_ATTR(fts_gesture_fod_pressed, S_IRUGO,
+                   fts_gesture_fod_pressed_show, NULL);
+
 static struct attribute *fts_gesture_mode_attrs[] = {
 	&dev_attr_fts_gesture_mode.attr,
 	&dev_attr_fts_gesture_buf.attr,
 	&dev_attr_fts_gesture_bm.attr,
 	&dev_attr_fts_gesture_point.attr,
 	&dev_attr_fts_gesture_single_tap_pressed.attr,
+	&dev_attr_fts_gesture_fod_pressed.attr,
 	NULL,
 };
 
@@ -300,6 +317,7 @@ void fts_fod_report_key(struct fts_ts_data *ts_data)
 	memset(&coordinate, 0, sizeof(coordinate));
 	if ((ts_data->fod_info.fp_down) && (!ts_data->fod_info.fp_down_report)) {
 		ts_data->fod_info.fp_down_report = 1;
+		sysfs_notify(&ts_data->dev->kobj, NULL, "fts_gesture_fod_pressed");
 		input_report_key(ts_data->input_dev, KEY_GESTURE_FOD, 1);
 		input_sync(ts_data->input_dev);
 		FTS_DEBUG("KEY_GESTURE_FOD, 1");
@@ -309,6 +327,7 @@ void fts_fod_report_key(struct fts_ts_data *ts_data)
 	}
 	else if ((!ts_data->fod_info.fp_down) && (ts_data->fod_info.fp_down_report)) {
 		ts_data->fod_info.fp_down_report = 0;
+		sysfs_notify(&ts_data->dev->kobj, NULL, "fts_gesture_fod_pressed");
 		input_report_key(ts_data->input_dev, KEY_GESTURE_FOD, 0);
 		input_sync(ts_data->input_dev);
 		FTS_DEBUG("KEY_GESTURE_FOD, 0");